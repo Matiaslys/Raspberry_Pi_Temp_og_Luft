34647cbf9d87e84327aca21678a2d6ed8892b3e8
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-08-27 14:19:48 +0200

    python server version 1

---
+++ src/python/Sensor_Server.py
@@ -0,0 +1,23 @@
+import socket
+import Adafruit_DHT
+
+HOST = '127.0.0.1'
+PORT = 8000
+gpio=17
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+if humidity is not None and temperature is not None:
+  print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+  print('Failed to get reading. Try again!')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        conn.sendall(humidity)
+        conn.sendall(temperature)
+        
+            
