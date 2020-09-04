31dc182c074a37c2a5f44b08a19ef8b26440075e
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-09-04 13:25:26 +0200

    Parse int til parse double

--- src/sample/Klient.java
+++ src/sample/Klient.java
@@ -7,16 +7,16 @@ import java.net.Socket;
 
 public class Klient {
     DataInputStream fromServer;
-    int temp;
-    int hum;
+    double temp;
+    double hum;
     public void recieveData() throws IOException {
-        Socket socket = new Socket("localhost", 8000);
+        Socket socket = new Socket("192.168.43.125", 8000);
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         fromServer = new DataInputStream(socket.getInputStream());
         String humid = stdIn.readLine();
         String in = stdIn.readLine();
-        temp = Integer.parseInt(in);
-        hum = Integer.parseInt(humid);
+        temp = Double.parseDouble(in);
+        hum = Double.parseDouble(humid);
         System.out.println(temp);
         System.out.println(hum);
     }
