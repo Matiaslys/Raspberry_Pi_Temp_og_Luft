76bf03f4ccbf78adf8c5c39032eab786f29fc259
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-09-04 13:47:38 +0200

    Add files via upload
    rykket alt igen

--- src/python/Sensor_Server_With_Loop.py
+++ src/python/Sensor_Server_With_Loop.py
@@ -29,21 +29,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(humidity)
         print(temperature)
         count = 0
-    while True:
-        time.sleep(delay)
-        humidity, temperature = Adafruit_DHT.read_retry(11, gpio)
-        if humidity is not None and temperature is not None:
-            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-        else:
-            print('Failed to get reading. Try again!')
-        count += 1
-        humidity2 = str(humidity).encode("utf-8")
-        temperature2 = str(temperature).encode("utf-8")
-        conn.send(humidity2)
-        conn.send('\n'.encode("utf-8"))
-        conn.send(temperature2)
-        conn.send('\n'.encode("utf-8"))
-        print(humidity)
-        print(temperature)
+        while True:
+            time.sleep(delay)
+            humidity, temperature = Adafruit_DHT.read_retry(11, gpio)
+            if humidity is not None and temperature is not None:
+                print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+            else:
+                print('Failed to get reading. Try again!')
+            count += 1
+            humidity2 = str(humidity).encode("utf-8")
+            temperature2 = str(temperature).encode("utf-8")
+            conn.send(humidity2)
+            conn.send('\n'.encode("utf-8"))
+            conn.send(temperature2)
+            conn.send('\n'.encode("utf-8"))
+            print(humidity)
+            print(temperature)
         
             
