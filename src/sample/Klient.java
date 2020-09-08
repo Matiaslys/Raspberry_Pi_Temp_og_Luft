e6dd810c4bbf5eac9cd37faf2ace8406bfe37e73
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-09-08 15:26:37 +0200

    Update Klient.java
    tilføjet kommentarer

--- src/sample/Klient.java
+++ src/sample/Klient.java
@@ -10,20 +10,25 @@ public class Klient {
     private double hum;
     public void recieveData(SensorData sensorData) throws IOException {
         new Thread(new Runnable() {
+            // prøver og få connection til serveren.
             Socket socket = new Socket("192.168.43.125", 8000);
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             @Override
             public void run() {
                 try {
                     while (true) {
+                        // får information hele tiden imens vi har en connection.
                         String humid = stdIn.readLine();
                         String in = stdIn.readLine();
+                        // sout til at som vi får den rigtige information.
                         System.out.println(humid);
                         System.out.println(in);
+                        // laver information om til double så vi kan bruge den.
                         temp = Double.parseDouble(in);
                         hum = Double.parseDouble(humid);
                         System.out.println("temp " + temp);
                         System.out.println("hum " + hum);
+                        // sætter informationen i vores klasse
                         sensorData.setTemp(temp);
                         sensorData.setHum(hum);
 
@@ -32,6 +37,7 @@ public class Klient {
                     e.printStackTrace();
                 }
             }
+            // starter tråden
         }).start();
         }
     }
