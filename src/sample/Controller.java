412674b572850040f998604fbf6d8d50f45cff38
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-09-08 15:22:51 +0200

    Update Controller.java
    Tilføjet kommentarer

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,7 +14,7 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class Controller {
-
+    // fxml variabler og id.
     @FXML
     private LineChart<Number, Number> tempchart;
 
@@ -35,15 +35,18 @@ public class Controller {
 
 
     public void Chart(SensorData sensorData) throws IOException {
+        // laver 2 instancer af XYchart og giver dem data.
         tempChartSeries = new XYChart.Series<Number, Number>();
         humChartSeries = new XYChart.Series<Number, Number>();
         tempChartSeries.setName("Temperatur");
         humChartSeries.setName("Luftfugtighed");
         tempchart.getData().add(tempChartSeries);
         humchart.getData().add(humChartSeries);
+        // tilføjer en Observer.
         sensorData.addObserver(new Observer() {
             @Override
             public void update(Observable o, Object arg) {
+                // ændre vores GUI på en ny tråd.
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
@@ -52,17 +55,22 @@ public class Controller {
                 });
             }
         });
+        // laver en ny instans af Klient og giver den vores sensorData som parameter.
         Klient klient = new Klient();
         klient.recieveData(sensorData);
     }
 
     private void InsertTempAndHum(SensorData sensorData) {
         try {
+            // bruger localtime klassen til at få hvad klokken er lige nu i forhold til sekunder.
             LocalTime localTime = LocalTime.now();
             double time = (double) localTime.getSecond();
+            // bruger localDateTime klassen til at få dato og klokkeslæt.
             LocalDateTime myDateObj = LocalDateTime.now();
+            // formatter det så det ser pænt ud.
             DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
             String formattedDate = myDateObj.format(myFormatObj);
+            // giver vores chart data og tilføjer tekst til vores labels.
             tempChartSeries.getData().add(new XYChart.Data<Number, Number>(time, sensorData.getTemp()));
             humChartSeries.getData().add(new XYChart.Data<Number, Number>(time, sensorData.getHum()));
             dato.setText("Tidspunkt for sidste aflæsning " + formattedDate);
