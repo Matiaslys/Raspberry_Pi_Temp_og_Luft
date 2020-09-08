2aa78b78c7f694316aaffe20a99f2d9f420c63c2
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-09-08 15:13:44 +0200

    Update Main.java
    Skrevet nogle kommentarer

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,12 +11,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // laver en ny instance af SensorData, som vi kan bruge når vi får vores data.
         SensorData sensorData = new SensorData();
+        // får vores fxml fil.
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
         Parent mineForloebParent = (Parent) fxmlLoader.load();
         Controller controller = fxmlLoader.getController();
+        // sender dataen fra vores SensorData klasse videre
         controller.Chart(sensorData);
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Temp og Luft");
         primaryStage.setScene(new Scene(mineForloebParent, 1100, 600));
         primaryStage.show();
     }
