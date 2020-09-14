fe91743b3c0ff029eec93d1f111cd5c90ea04732
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-09-14 16:42:16 +0200

    Add files via upload

--- src/python/Sensor_Server_With_Encryption.py
+++ src/python/Sensor_Server_With_Encryption.py
@@ -15,7 +15,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
-        obj = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
+        obj = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV4567')
         humidity, temperature = Adafruit_DHT.read_retry(11, gpio)
         if humidity is not None and temperature is not None:
             print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
@@ -26,7 +26,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         humidity2 = str(humidity).encode("utf-8")
         temperature2 = str(temperature).encode("utf-8")
         ciphertext = obj.encrypt(humidity2)
+        print(ciphertext)
         ciphertext2 = obj.encrypt(temperature2)
+        print(ciphertext2)
         conn.send(ciphertext)
         conn.send('\n'.encode("utf-8"))
         conn.send(ciphertext2)
